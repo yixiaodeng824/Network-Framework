b2c1d972db530b0b23378d0c31d1751c408e0f90
yixiaodeng <3077351472@qq.com>
2026-08-14 11:37:51 +0800

    添加命令行参数，支持启动初始化

--- src/net/main.cpp
+++ src/net/main.cpp
@@ -1,11 +1,35 @@
 #include "epoll_server.h"
 #include "Logger.h"
+#include <cstdlib>
+#include <cstring>
 using namespace std;
 
-int main() {
+int main(int argc,char* argv[]) {
 	logger::setLogLevel(logger::LOG_LEVEL_DEBUG);
-	ThreadPool tp(4);
-	EpollServer es(8888,tp);
+	int port = 8888;
+	int threadnum = 0;
+	for (int i = 1; i < argc; i++) {
+		if ((strcmp(argv[i], "-p") == 0 || (strcmp(argv[i], "--port") == 0) )){
+			if (i+1 >= argc) {
+				LOG_DEBUG("no port");
+				break;
+			}
+			else {
+				port = atoi(argv[i + 1]);
+			}
+		}
+		if ((strcmp(argv[i], "-t") == 0 || (strcmp(argv[i], "--threads") == 0))) {
+			if (i+1 >= argc) {
+				LOG_DEBUG("no threadnum");
+				break;
+			}
+			else {
+				threadnum = atoi(argv[i + 1]);
+			}
+		}
+	}
+	ThreadPool tp(threadnum);
+	EpollServer es(port, tp);
 	es.start();
 	return 0;
 }
